af17f367eb6b55f797717ded9985454ce94a5f5a
nakaji-dayo <nakaji.dayo@gmail.com>
2019-04-01 20:16:16 +0900

    Support for Reiwa

--- src/Data/Time/Calendar/Japanese.hs
+++ src/Data/Time/Calendar/Japanese.hs
@@ -3,7 +3,7 @@ module Data.Time.Calendar.Japanese where
 import           Data.List
 import qualified Data.Time.Calendar as C
 
-data Gengo = Meiji | Taisho | Showa | Heisei
+data Gengo = Meiji | Taisho | Showa | Heisei | Reiwa
   deriving (Eq, Show, Read, Bounded, Enum)
 
 data Day = Day Gengo Integer Int Int
@@ -14,12 +14,10 @@ data Day = Day Gengo Integer Int Int
 -- Supports Meiji,Taisho,Showa,Heisei.
 -- It returns Nothing if out of support range.
 fromDay :: C.Day -> Maybe Day
-fromDay day
-  | day >= endOfSupport = Nothing
-  | otherwise =
-    let ms = find (\(g, s) -> day >= s) $ (\g -> (g, startDayOf g))
-             <$> reverse [minBound .. maxBound]
-    in uncurry fromBaseDay <$> ms
+fromDay day =
+  let ms = find (\(g, s) -> day >= s) $ (\g -> (g, startDayOf g))
+           <$> reverse [minBound .. maxBound]
+  in uncurry fromBaseDay <$> ms
   where
     (y, m, d) = C.toGregorian day
     fromBaseDay g s =
@@ -32,17 +30,16 @@ toDay :: Day -> C.Day
 toDay (Day g y m d) = C.fromGregorian (y + y' - 1) m d
   where (y', _, _) = C.toGregorian (startDayOf g)
 
-endOfSupport :: C.Day
-endOfSupport = C.fromGregorian 2019 5 1
-
 startDayOf :: Gengo -> C.Day
 startDayOf Heisei = C.fromGregorian 1989 1 8
 startDayOf Showa  = C.fromGregorian 1926 12 26
 startDayOf Taisho = C.fromGregorian 1912 7 30
 startDayOf Meiji  = C.fromGregorian 1868 9 4
+startDayOf Reiwa  = C.fromGregorian 2019 5 1
 
 format :: Gengo -> String
 format Heisei = "平成"
 format Showa  = "昭和"
 format Taisho = "大正"
 format Meiji  = "明治"
+format Reiwa  = "令和"
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,7 @@ instance Arbitrary Day where
   arbitrary = do
     g <- arbitraryBoundedEnum
     let f = startDayOf g
-    (y,m,d) <- C.toGregorian . C.ModifiedJulianDay <$> choose (C.toModifiedJulianDay f, C.toModifiedJulianDay endOfSupport)
+    (y,m,d) <- C.toGregorian . C.ModifiedJulianDay <$> choose (C.toModifiedJulianDay f, C.toModifiedJulianDay limitDate)
     return $ Day g y m d
 
 main :: IO ()
@@ -20,14 +20,20 @@ main = hspec $ do
       fromDay (read "2018-09-11") `shouldBe` Just (Day Heisei 30 9 11)
     it "my birthday" $
       fromDay (read "1989-01-08") `shouldBe` Just (Day Heisei 1 1 8)
-    it "not supported" $
-      fromDay (read "2019-05-01") `shouldBe` Nothing
+    it "from 2019-05-01" $
+      fromDay (read "2019-05-01") `shouldBe` Just (Day Reiwa 1 5 1)
   describe "toDay" $ do
       it "H16/2/9" $
         toDay (Day Heisei 16 2 29) `shouldBe` read "2004-02-29"
+      it "R16/2/9" $
+        toDay (Day Reiwa 16 2 28) `shouldBe` read "2034-02-28"
   prop "convert" $ forAll genSupportedDay $ \x ->
     toDay <$> fromDay x `shouldBe` Just x
 
 genSupportedDay = C.ModifiedJulianDay <$>
   choose ( C.toModifiedJulianDay (startDayOf Meiji)
-         , C.toModifiedJulianDay endOfSupport - 1)
+         , C.toModifiedJulianDay limitDate - 1)
+
+
+limitDate :: C.Day
+limitDate = C.fromGregorian 2100 1 1
